2dcce8328d671e3ec1d1c83810712a6159838365
ayodelehallymah <ayodelehallymah@gmail.com>
2023-06-03 16:26:42 -1200

    edited

--- index.js
+++ index.js
@@ -1,40 +1,31 @@
 const body = document.body
-const div = document.getElementById("container")
-const pic = document.getElementsByClassName('pic')
-const fullName = document.getElementsByClassName('fullName')
-const phone = document.getElementsByClassName('phone')
-const email = document.getElementsByClassName('email')
-const address = document.getElementsByClassName('address')
+const myDiv = document.getElementById('myDiv')
+const imageSrc = document.getElementById('imageSrc')
+const fullName = document.getElementById('fullName')
+const address = document.getElementById('address')
+const phonenumber = document.getElementById('phoneNumber')
+const email = document.getElementById('email')
+const title = document.getElementById('title')
+const url =  "https://randomuser.me/api/"
 const btn = document.getElementsByClassName('btn')
+title.textContent = 'RANDOM CARD GENERATOR'
 
-btn.addEventListener('click', () =>{
-    const url = "https://randomuser.me/api/"
-    const cardInfo = () => {
-        fetch(url)
-        .then(response => {
-            return response.json();
-        })
-        .then(data => {
-            const cardImage = data.results[0].picture.medium
-            image.src = cardImage 
+
+ fetch(url).then(response => response.json())
+ .then(data => {
     
-            const cardTitle = data.results[0].name.title + data.results[0].name.first + data.results[0].name.last
-            cardTitle.textContent = cardTitle
-    
-            const cardMail = data.results[0].email
-            cardMail.textContent = cardMail
-    
-            const cardAdd = data.results.location.street.number.name
-            address.textContent = address
+    const cardImg = data.results[0].picture.large
+     imageSrc.src = cardImg
 
-            console.log(cardAdd);
-        })
-    }
-    
-}
-)
+    const cardFullName = data.results[0].name.title + (' ') + data.results[0].name.first + (' ') + data.results[0].name.last
+   fullName.textContent = cardFullName
 
-cardInfo()
+    const phoneNum = 'Phone Number' + (' - ') + data.results[0].phone
+    phonenumber.textContent = phoneNum
 
+    const emailAdd = 'Email-Address -' + (' ') +  data.results[0].email
+    email.textContent = emailAdd
 
-
+    const cardAddress =  'Address -' + (' ') + data.results[0].location.street.number + (', ') + data.results[0].location.street.name
+    address.textContent = cardAddress
+ })
